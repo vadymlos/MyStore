fac13210d40b54768a42d6f0e4e99e0d8241a2cf
vadymlos <vadym.los@pdffiller.team>
2020-09-05 19:02:21 +0300

    Add BaseTest

--- src/test/java/UI/BaseTest.java
+++ src/test/java/UI/BaseTest.java
@@ -1,8 +1,24 @@
 package UI;
 
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeClass;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import driver.SelenoidDriverProvider;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest {
 
-
-
-
+    @BeforeClass
+    public void BeforeMethod(){
+        Configuration.baseUrl = "http://172.32.64.34";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = SelenoidDriverProvider.class.getName();
+        Configuration.fastSetValue = true;
+        Configuration.driverManagerEnabled = false;
+        //   open("/index.html");
+//        RestAssured.port = 80;
+    }
 }
